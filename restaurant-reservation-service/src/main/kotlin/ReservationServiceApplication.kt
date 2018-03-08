aac5e1f2ef9e96d8944265a1a2f7176c73bcd202
Hendi Santika <hendisantika@gmail.com>
2018-03-09 06:42:51 +0700

    #4 - Add main App in restaurant-reservation-service

---
+++ restaurant-reservation-service/src/main/kotlin/ReservationServiceApplication.kt
@@ -0,0 +1,20 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : restaurant-application
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/03/18
+ * Time: 06.42
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+class ReservationServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReservationServiceApplication>(*args)
+}
